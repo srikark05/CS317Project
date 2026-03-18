0e0a26972e8130a0050bc07986bee06f656e2a8e
BraxtonLaNois <LanoisBraxton@gmail.com>
2026-03-18 18:42:52 +0000

    added stats

--- Database/db/create.sql
+++ Database/db/create.sql
@@ -7,10 +7,29 @@ CREATE TABLE Team (
     owner VARCHAR(100),
     tv_tag VARCHAR(3)
 );
+CREATE TABLE Positions (
+    position VARCHAR(2) PRIMARY KEY
+);
+
+CREATE TABLE Offense (
+    position VARCHAR(2) PRIMARY KEY,
+    FOREIGN KEY (position) REFERENCES Positions(position)
+);
+
+CREATE TABLE Defense (
+    position VARCHAR(2) PRIMARY KEY,
+    FOREIGN KEY (position) REFERENCES Positions(position)
+);
+
+CREATE TABLE Special_Teams (
+    position VARCHAR(2) PRIMARY KEY,
+    FOREIGN KEY (position) REFERENCES Positions(position)
+);
 
 CREATE TABLE Player (
     name VARCHAR(100),
     dob DATE,
+    position VARCHAR(2),
     number INT,
     weight INT,
     height INT,
@@ -68,6 +87,7 @@ CREATE TABLE PlaysFor (
     player_dob DATE,
     team_name VARCHAR(100),
     season INT,
+    
 
     PRIMARY KEY (player_name, player_dob, team_name, season),
 
@@ -76,6 +96,8 @@ CREATE TABLE PlaysFor (
 
     FOREIGN KEY (team_name)
         REFERENCES Team(name)
+
+     FOREIGN KEY  (position) REFERENCES Positions(position)
 );
 
 
@@ -94,6 +116,42 @@ CREATE TABLE CoachesFor (
         REFERENCES Team(name)
 );
 
+Create Table season_stats(
+    player_name VARCHAR(100),
+    player_dob DATE,
+    season INT,
+    season_rushing_yards INT,
+    season_rushing_attempts INT,
+    season_rushing_touchdowns INT,
+    season_receiving_yards INT,
+    season_receiving_attempts INT,
+    season_receiving_touchdowns INT,
+    season_passing_yards INT,
+    season_passing_attempts INT,
+    season_passing_completions INT,
+    season_passing_touchdowns INT,
+    season_offensive_interceptions INT,
+    season_defensive_interceptions INT,
+    season_offensive_sacks INT,
+    season_defensive_sacks INT,
+    season_tackles INT,
+    season_tackles_for_loss INT,
+    season_forced_fumbles INT,
+    season_fumble_recoveries INT,
+    season_special_teams_returns INT,
+    season_special_teams_touchdowns INT,
+    season_special_teams_yards INT,
+    season_punting_yards INT,
+    season_punting_attempts INT,
+    season_kicking_attempts INT,
+    season_kicking_made INT,
+    season_extra_point_attempts INT,
+    season_extra_points_made INT,
+
+     PRIMARY KEY (player_name, player_dob,season),
+
+     FOREIGN KEY (player_name, player_dob)
+        REFERENCES Player(name, dob);
 
 CREATE TABLE Played_In (
     player_name VARCHAR(100),
@@ -101,7 +159,33 @@ CREATE TABLE Played_In (
     game DATE,
     Week INT,
     home_team VARCHAR(100)
-
+    game_rushing_yards INT,
+    game_rushing_attempts INT,
+    game_rushing_touchdowns INT,
+    game_receiving_yards INT,
+    game_receiving_attempts INT,
+    game_receiving_touchdowns INT,
+    game_passing_yards INT,
+    game_passing_attempts INT,
+    game_passing_completions INT,
+    game_passing_touchdowns INT,
+    game_offensive_interceptions INT,
+    game_defensive_interceptions INT,
+    game_offensive_sacks INT,
+    game_defensive_sacks INT,
+    game_tackles INT,
+    game_tackles_for_loss INT,
+    game_forced_fumbles INT,
+    game_fumble_recoveries INT,
+    game_special_teams_returns INT,
+    game_special_teams_touchdowns INT,
+    game_special_teams_yards INT,
+    game_punting_yards INT,
+    game_punting_attempts INT,
+    game_kicking_attempts INT,
+    game_kicking_made INT,
+    game_extra_point_attempts INT,
+    game_extra_points_made INT,
     PRIMARY KEY (player_name, player_dob, game),
 
     FOREIGN KEY (player_name, player_dob)
@@ -113,51 +197,4 @@ CREATE TABLE Played_In (
         REFERENCES Game(week)
     FOREIGN KEY (home_team)
         REFERENCES Game(home_team)
-);
-'I need to finish including the stats because idk how to set up the ISAs'
-
-CREATE TABLE Passing_Stat (
-    stat_id INT PRIMARY KEY,
-    player_name VARCHAR(100),
-    player_dob DATE,
-    game_id INT,
-    yards INT,
-    touchdowns INT,
-    interceptions INT,
-
-    FOREIGN KEY (player_name, player_dob)
-        REFERENCES Player(name, dob),
-
-    FOREIGN KEY (game_id)
-        REFERENCES Games(game_id)
-);
-
-CREATE TABLE Rushing_Stat (
-    stat_id INT PRIMARY KEY,
-    player_name VARCHAR(100),
-    player_dob DATE,
-    game_id INT,
-    yards INT,
-    touchdowns INT,
-
-    FOREIGN KEY (player_name, player_dob)
-        REFERENCES Player(name, dob),
-
-    FOREIGN KEY (game_id)
-        REFERENCES Games(game_id)
-);
-
-CREATE TABLE Receiving_Stat (
-    stat_id INT PRIMARY KEY,
-    player_name VARCHAR(100),
-    player_dob DATE,
-    game_id INT,
-    yards INT,
-    touchdowns INT,
-
-    FOREIGN KEY (player_name, player_dob)
-        REFERENCES Player(name, dob),
-
-    FOREIGN KEY (game_id)
-        REFERENCES Games(game_id)
 );
